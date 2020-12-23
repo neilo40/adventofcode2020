6fb580d234c28594f5afd941bd3ca319f873daaa
neil <neil@neiloconnor.com>
2020-12-23 13:11:11 +0000

    day 15

--- cmd/day15/main.go
+++ cmd/day15/main.go
@@ -12,29 +12,60 @@ type Number struct {
 }
 
 func main() {
-	part1()
-	//part2()
+	//part1()
+	part2()
 }
 
 func part1() {
-	var input = []int64{0, 1, 12, 0, 20, 8, 16}
-	game := make(map[int64]Number)
+	// input is 1,12,0,20,8,16 (16 is handled below)
+	var input = []int64{0, 1, 12, 0, 20, 8}
+	game := make(map[int64]*Number)
 	for i, n := range input {
-		game[n] = Number{n, int64(i)}
+		game[n] = &Number{n, int64(i + 1), int64(i + 1)}
 	}
-	currentNumber := input[6]
-	for i := 7; i < 2021; i++ {
-		n, ok := game[currentNumber]
-		if !ok {
-			currentNumber = 0
-		} else {
 
+	currentNumber := int64(16)
+	var newCurrentNumber int64
+
+	for i := 7; i < 2021; i++ {
+		_, ok := game[currentNumber]
+		if !ok {
+			game[currentNumber] = &Number{currentNumber, int64(i), int64(i)}
+			newCurrentNumber = 0
+		} else {
+			game[currentNumber].LastSpokenPrev = game[currentNumber].LastSpoken
+			game[currentNumber].LastSpoken = int64(i)
+			newCurrentNumber = game[currentNumber].LastSpoken - game[currentNumber].LastSpokenPrev
 		}
+		currentNumber = newCurrentNumber
 	}
+
 	fmt.Printf("2020th number spoken is %d\n", currentNumber)
 }
 
 func part2() {
-	//lines := common.ReadFileString("dayX.input")
-	fmt.Printf("Result: \n")
+	// input is 1,12,0,20,8,16 (16 is handled below)
+	var input = []int64{0, 1, 12, 0, 20, 8}
+	game := make(map[int64]*Number)
+	for i, n := range input {
+		game[n] = &Number{n, int64(i + 1), int64(i + 1)}
+	}
+
+	currentNumber := int64(16)
+	var newCurrentNumber int64
+
+	for i := 7; i < 30000001; i++ {
+		_, ok := game[currentNumber]
+		if !ok {
+			game[currentNumber] = &Number{currentNumber, int64(i), int64(i)}
+			newCurrentNumber = 0
+		} else {
+			game[currentNumber].LastSpokenPrev = game[currentNumber].LastSpoken
+			game[currentNumber].LastSpoken = int64(i)
+			newCurrentNumber = game[currentNumber].LastSpoken - game[currentNumber].LastSpokenPrev
+		}
+		currentNumber = newCurrentNumber
+	}
+
+	fmt.Printf("30000000th number spoken is %d\n", currentNumber)
 }
